4892dd032589288f6f7b784d6f214b24f9f00a0b
moqiloveboa <1120959931@qq.com>
2016-11-20 04:30:33 +0800

    mysql class

--- front/lib/Mysql.class.php
+++ front/lib/Mysql.class.php
@@ -4,10 +4,16 @@
 */
 class Mysql
 {
-	
-	function __construct()
+	public $mysql_host;
+	public $mysql_user;
+	public $mysql_pass;
+	public $mysql_db;
+	function __construct($host,$user,$pass,$db)
 	{
-		
+		$this->mysql_host 	= $host;
+		$this->mysql_user 	= $user;
+		$this->mysql_pass 	= $pass;
+		$this->mysql_db 	= $db;
 	}
 }
 
